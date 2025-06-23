b619960ad254fd6f9c1b14cf4f865eac437021d2
muhammadullah1 <muhammadullah403@gmail.com>
2025-06-23 19:45:46 +0500

    Trigger deploy

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require("./app");
 const port = process.env.PORT || 8080
 app.listen(port, () => {
-  console.warn(`✅  Server running on port: ${port}`);
+  console.warn(`✅  Server running on port updated: ${port}`);
 });
